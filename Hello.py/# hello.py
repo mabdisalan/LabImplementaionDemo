adba4202f2558fb32ef2fe793766abb5230d1f09
mabdisalan <mabdisalan39@gmail.com>
2025-10-04 16:13:42 +0300

    Create # hello.py

---
+++ Hello.py/# hello.py
@@ -0,0 +1,8 @@
+# hello.py
+# This is a simple Python script for lab activities demo
+
+def greet():
+    print("Hello, this is my first GitHub Desktop project!")
+
+if __name__ == "__main__":
+    greet()
